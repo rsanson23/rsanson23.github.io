2597aceee974e9ecda40c56f8acde2c40ddade97
Robert Sanson <23rsanson23@gmail.com>
2021-04-22 15:52:46 -0500

    Added opacity change to side navigation bar

--- scripts/sideNav.js
+++ scripts/sideNav.js
@@ -1,8 +1,10 @@
 // Expands side navigation bar to fixed width and show links to projects
 const openSideNav = () => {
     document.getElementById('sideNavDiv').style.width = "200px";
+    document.getElementById('sideNavDiv').style.backgroundColor = "rgba(70, 70, 70, 0.7)"
     document.getElementById('side-btn').textContent = "-";
     document.getElementById('side-btn').style.marginLeft = "180px";
+    document.getElementById('side-btn').style.backgroundColor = "rgba(70, 70, 70, 0.7)"
     document.getElementById('side-btn').setAttribute("onclick", "javascript: closeSideNav();");
     var content = document.getElementsByClassName('sideNavContent');
 
@@ -15,8 +17,10 @@ const openSideNav = () => {
 // Contracts side navigation bar and hides links to projects
 const closeSideNav = () => {
     document.getElementById('sideNavDiv').style.width = "20px";
+    document.getElementById('sideNavDiv').style.backgroundColor = "rgba(70, 70, 70, 0)"
     document.getElementById('side-btn').textContent = "+";
     document.getElementById('side-btn').style.marginLeft = "0";
+    document.getElementById('side-btn').style.backgroundColor = "rgba(70, 70, 70, 0)"
     document.getElementById('side-btn').setAttribute("onclick", "javascript: openSideNav();");
 
     var content = document.getElementsByClassName('sideNavContent');

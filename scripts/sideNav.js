fb9c24a43a76596d11f08d2592c45c966b02883f
Robert Sanson <23rsanson23@gmail.com>
2021-04-28 13:11:53 -0500

    Various changes to style and linking of website

--- scripts/sideNav.js
+++ scripts/sideNav.js
@@ -3,6 +3,7 @@ const openSideNav = () => {
     document.getElementById('sideNavDiv').style.width = "200px";
     document.getElementById('sideNavDiv').style.backgroundColor = "rgba(70, 70, 70, 0.7)"
     document.getElementById('side-btn').textContent = "-";
+    document.getElementById('side-btn').style.fontSize = "20px";
     document.getElementById('side-btn').style.marginLeft = "180px";
     document.getElementById('side-btn').style.backgroundColor = "rgba(70, 70, 70, 0.7)"
     document.getElementById('side-btn').setAttribute("onclick", "javascript: closeSideNav();");
@@ -16,9 +17,10 @@ const openSideNav = () => {
 
 // Contracts side navigation bar and hides links to projects
 const closeSideNav = () => {
-    document.getElementById('sideNavDiv').style.width = "20px";
+    document.getElementById('sideNavDiv').style.width = "40px";
     document.getElementById('sideNavDiv').style.backgroundColor = "rgba(70, 70, 70, 0)"
     document.getElementById('side-btn').textContent = "+";
+    document.getElementById('side-btn').style.fontSize = "40px";
     document.getElementById('side-btn').style.marginLeft = "0";
     document.getElementById('side-btn').style.backgroundColor = "rgba(70, 70, 70, 0)"
     document.getElementById('side-btn').setAttribute("onclick", "javascript: openSideNav();");
